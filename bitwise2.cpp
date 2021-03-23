2ee91b1ae00bc05c364fb804c4d956b681484379
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-03-23 13:14:45 -0400

    Add files via upload

---
+++ bitwise2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // 1. Bitwise left shift operator
+    // int x = 4;
+    // cout<<(x<<1)<<endl;
+
+    // cout<<(x<<2)<<endl;
+
+    
+    
+    
+    // 2. Bitwise right shift operator
+    // int x = 4;
+    // cout<<(x>>1)<<endl;
+
+    // cout<<(x>>2)<<endl;
+
+    // 3.bitwise not operator unsigned int
+    // unsigned int x = 8;
+    // cout << ~x << endl;
+
+    // //4.bitwise not operator signed int
+
+    int x = -5;
+    cout << ~x << endl;
+
+    // cout<<pow(2,3);
+    return 0;
+}
