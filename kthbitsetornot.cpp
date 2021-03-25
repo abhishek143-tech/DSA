70120749a4609db858494e5703e9f8ef8f6f3f56
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-03-25 12:43:30 -0400

    Add files via upload

---
+++ kthbitsetornot.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+      int n,k;
+      cout<<"Enter the value of n and k\n";
+      cin>>n>>k;
+      //using bitwise left shift operator
+      // if(n&(1<<k-1) !=0){
+      //      cout<<"YES"<<endl;
+      // }else{
+      //      cout<<"NO"<<endl;
+      // }
+      
+      //using bitwise right shift operator
+      if(n>>(k-1)&1==1){
+           cout<<"YES\n";
+      }else{
+           cout<<"NO\n";
+      }
+      return 0;
+}
